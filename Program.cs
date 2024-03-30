a6f5ab511ffbcbe9ff5b76a021f5c867ba93ac65
kissb364 <160039734+kissb364@users.noreply.github.com>
2024-03-30 14:38:37 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,31 +2,63 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace Masodfoku
+namespace Taskok
 {
-    public class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        static async Task Egyes(CancellationToken cancellationToken)
         {
-            
-            
-        }
-        public static string MfEgyenlet(double a, double b, double c)
-        {
-            if (a == 0) throw new Exception("Nem másodfokú!");
-            else
+            while (!cancellationToken.IsCancellationRequested)
             {
-                double diszkr = b * b - 4 * a * c;
-                if (diszkr < 0) return "Nincs valós gyök!";
-                else
-                {
-                    double x1 = (-b + Math.Sqrt(diszkr)) / (2 * a);
-                    double x2 = (-b - Math.Sqrt(diszkr)) / (2 * a);
-                    return x1.ToString("0.00") + ' ' + x2.ToString("0.00");
-                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("*");
+                await Task.Delay(100);
             }
+        }
+
+        static async Task Kettes(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("x");
+                await Task.Delay(100);
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("A task-ok elindultak.");
+            Console.WriteLine("\nNyomj meg egy billentyűt a task-ok leállításához...");
+
+            using (var cancellationTokenSource = new CancellationTokenSource())
+            {
+                // Várakozás a gombnyomásra a leállításhoz
+                var keyPressedTask = Task.Run(() =>
+                {
+                    Console.ReadKey();
+                });
+
+                var token = cancellationTokenSource.Token;
+
+                var taskEgyes = Egyes(token);
+                var taskKettes = Kettes(token);
+
+                // Várakozás arra, hogy bármelyik task befejeződjön
+                await Task.WhenAny(keyPressedTask, Task.WhenAll(taskEgyes, taskKettes));
+
+                // Ha a felhasználó lenyomta a billentyűt, leállítjuk a task-okat
+                cancellationTokenSource.Cancel();
+
+                // Várakozás a task-ok befejeződésére
+                await Task.WhenAll(taskEgyes, taskKettes);
+            }
+                Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("\nA task-ok leálltak.");
+            Thread.Sleep(3000);
         }
     }
 }
